822d5445e0bddbf0be081c540c96a81944c87da5
Mikko Aspelund <mikkoaspelund1@kamk.fi>
2022-09-08 11:23:13 +0300

    lmao

--- Project1/tehtävä2.cpp
+++ Project1/tehtävä2.cpp
@@ -2,17 +2,21 @@
 
 template <typename T>
 
-T EtsiP(T taulukko, int p)
+T EtsiP(T taulukko[], int p)
 {
-	T EtsiPienin = taulukko[0]
+	T pienin = taulukko[0];
 
+		for (int i = 1; i < p; i++)
+		{
+			if (taulukko[i] < pienin)
+			{
+				pienin = taulukko[i];
+			}
+		}
 
-
-		return EtsiPienin;
+		return pienin;
 };
-
 int main()
 {
-
-}
+};
 
